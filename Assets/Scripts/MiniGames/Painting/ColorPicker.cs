aa6b2369b7d6e61ce85fb76ec97c07e07e09bb05
FearWolf101 <FearElementStudio@gmail.com>
2021-09-17 16:41:19 -0700

    Started painting game
    I worked on getting the functionality going and made it where the colors change and you can erase the paint.

---
+++ Assets/Scripts/MiniGames/Painting/ColorPicker.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorPicker : MonoBehaviour
+{ 
+     void OnMouseDown()
+    {
+        //take the color and drop it into the variable
+        PaintGM.currentColor = GetComponent<SpriteRenderer>().color;
+        Debug.Log(PaintGM.currentColor);
+        //increases the order of the color so there is no flicker with erasing.
+        //PaintGM.currentOrder += 1;
+    }
+
+}
---
+++ Assets/Scripts/MiniGames/Painting/dotControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dotControl : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().color = PaintGM.currentColor;
+        GetComponent<Transform>().localScale = new Vector2(PaintGM.currentScale, PaintGM.currentScale);
+    }
+    void OnMouseOver()
+    {
+        //if you click on the earser icon do this
+       if(PaintGM.toolType == "eraser")
+        Destroy(gameObject);
+
+    }
+}
