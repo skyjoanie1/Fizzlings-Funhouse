5808d757092f83f2141b5c84eaae6eef9669c170
Rich Reed <fearelementstudio@gmail.com>
2021-10-01 15:56:33 -0700

    painting minigame select colors
    I created the brushes and you can select what color you would like to use to draw with.

---
+++ Assets/Scripts/MiniGames/Painting/DrawSelector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DrawSelector : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public testDraw Mouse;
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Mouse.Selecting = false;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+            Mouse.Selecting = true;
+            Mouse.redBrushActive = false;
+            Mouse.greenBrushActive = false;
+            Mouse.purpleBrushActive = false;
+            Mouse.whiteBrushActive = false;
+            Mouse.orangeBrushActive = false;
+            Mouse.yellowBrushActive = false;
+            Mouse.blueBrushActive = false;
+            Mouse.brownBrushActive = false;
+            Mouse.blackBrushActive = false;
+    }
+}
