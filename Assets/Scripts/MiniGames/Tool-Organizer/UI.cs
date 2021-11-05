28cebe6a703a2e2af9da4d3393fcbc0a12aed157
Rich Reed <fearelementstudio@gmail.com>
2021-11-05 15:11:48 -0700

    Tool Game
    Created the tools minigame

---
+++ Assets/Scripts/MiniGames/Tool-Organizer/UI.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    //Variables Game Over UI
+    public GameObject gameOverBackground;
+    public Text gameOverText;
+
+    //Variables for the hub button and play again buttons
+    public GameObject hubButton;
+    public int totalTools = 7;
+    public Text toolsTotalText;
+    void Start()
+    {
+        DeactivateUI();
+       
+    }
+
+    //Function to activate UI
+    public void ActivateUI()
+    {
+        gameOverBackground.SetActive(true);
+        gameOverText.enabled = true;
+        hubButton.SetActive(true);
+       
+    }
+
+    //Function to deactivate UI
+    public void DeactivateUI()
+    {
+        gameOverBackground.SetActive(false);
+        gameOverText.enabled = false;
+        hubButton.SetActive(false);
+    }
+
+    public void Update()
+    {
+        toolsTotalText.text = "Points:" + GameManager.Instance.ToolsTotalPoints.ToString();
+       
+        if(GameManager.Instance.ToolsTotalPoints == totalTools)
+        {
+            ActivateUI();
+        }
+    }
+}
+
+
