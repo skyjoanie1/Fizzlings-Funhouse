2c4779ead65f31f17e472bb4b69a7173c617c4fd
va2678526 <darkangelalways5@aol.com>
2022-02-04 15:33:15 -0700

    HUB + Mouse over
    Finished setting up the HUB (in a completely new scene just to make sure there are no conflicts), imported a proper png for the Ocean Room Door.  Also got started on a UI script that will show the name of an object before it is clicked

---
+++ Assets/Scripts/MouseOver.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MouseOver : MonoBehaviour
+{
+
+    public void OnMouseOver()
+    {
+        //If your mouse hovers over the GameObject with the script attached, output this message
+        Debug.Log("Mouse is over " + this.name);
+    }
+
+    public void OnMouseExit()
+    {
+        //The mouse is no longer hovering over the GameObject so output this message each frame
+        Debug.Log("Mouse is no longer on GameObject.");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Hovering");
+    }
+}
