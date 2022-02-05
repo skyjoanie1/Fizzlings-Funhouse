56fe47d6910beb516b6658fba4853757e5969561
Samuel McKinley <sammckin@uat.edu>
2022-02-04 17:26:07 -0700

    Minigame is Functional
    With the addition of the added scripts, the minigame is functional and ready

---
+++ Assets/Scripts/MiniGames/Galactic Diplomacy/AnswerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnswerScript : MonoBehaviour
+{
+    public bool isCorrect;
+    public QuizManager quizManager;
+    public void Answer()
+    {
+        if (isCorrect)
+        {
+            Debug.Log("Correct Answer!");
+            quizManager.correct();
+        }
+        else
+        {
+            Debug.Log("Incorrect Answer...");
+            quizManager.correct();
+        }
+    }
+}
